0d02b6c26abe876e9a9eae01c889f7cc9b26bc06
rhazem13 <rhazem13@yahoo.com>
2022-11-25 18:35:04 +0200

    لاى

--- app/etl/DataSources/Media/Media.py
+++ app/etl/DataSources/Media/Media.py
@@ -1,11 +1,12 @@
 from DataSources import IDataSource
+from app.bird_cv.core import extract_count_of_birds_from_video 
 
 class Media(IDataSource):
     def __init__(self, type) -> None:
         super().__init__(type)
 
     def extract(self, file_path):
-        print('extracting from media')
-
+        data = extract_count_of_birds_from_video(file_path)
+        return data
     def load(self, file_path):
-        print('loading from media')
+        print('not supported yet to load on media')
