c4f2f3483b1051d14641f205e20ddedf5ab95f34
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2022-11-25 18:35:50 +0200

    UpdateControllers

---
+++ app/Controllers/__init__.py
@@ -0,0 +1,14 @@
+from PyQt5 import QtWidgets
+from app.Controllers.ui import Ui_SQLCompiler
+import sys
+
+app = QtWidgets.QApplication(sys.argv)
+MainWindow = QtWidgets.QMainWindow()
+ui = Ui_SQLCompiler()
+ui.setupUi(MainWindow)
+
+# Connections
+from app.Controllers.controllers import compile, execute
+ui.combilebtn.clicked.connect(compile)
+ui.excutebtn.clicked.connect(execute)
+
