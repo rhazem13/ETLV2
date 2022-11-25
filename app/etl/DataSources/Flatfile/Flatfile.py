87954eab3d1cd92886d3b7843e5c0ed2ade439fb
shraf <eldeenshraf@gmail.com>
2022-11-25 14:14:32 +0200

    implement Flatfile

--- app/etl/DataSources/Flatfile/Flatfile.py
+++ app/etl/DataSources/Flatfile/Flatfile.py
@@ -1,10 +1,19 @@
 from DataSources import IDataSource
+from Flatfile.EFlatfile import EFlatfile
+import pandas as pd
 class Flatfile(IDataSource):
-    def __init__(self, type) -> None:
+    def __init__(self, type:EFlatfile) -> None:
         super().__init__(type)
+        self.callbacks = {
+            f'{EFlatfile.HTML}': (lambda path: pd.read_html(path)),
+            f'{EFlatfile.JSON}': (lambda path: pd.read_json(path)),
+            f'{EFlatfile.XML}': (lambda path: pd.read_xml(path)),
+            f'{EFlatfile.CSV}': (lambda path: pd.read_csv(path)),
+            f'{EFlatfile.EXCEL}': (lambda path: pd.read_excel(path))
+        }
 
     def load(self, file_path):
-        print('load flatfile')
+        self.callbacks[f'{self.type}'](file_path)
 
     def extract(self, file_path):
-        print('extract flatfile')
+        self.callbacks[f'{self.type}'](file_path)
