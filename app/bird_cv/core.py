5cb90859b184662b5ff1074b000913f1b70685a5
shraf <eldeenshraf@gmail.com>
2022-12-02 13:09:26 +0200

    add get_count function

--- app/bird_cv/core.py
+++ app/bird_cv/core.py
@@ -11,12 +11,10 @@ class BirdsCV:
     def extract_attrs_from_video(selv, filepath, atributes=None) -> pd.DataFrame:
         SENTINEL = object()
         queue = Queue()
-        current_directory = pathlib.Path(__file__).parent.resolve()
-        cascade_path = os.path.join(current_directory,'static','birds1.xml')
         attrs = dict()
         cap = cv2.VideoCapture(filepath)
         cv_reader = CVReader(cap, queue, SENTINEL)
-        cv_detector = CVDetector(cap, queue, cascade_path, lock, SENTINEL, attrs)
+        cv_detector = CVDetector(cap, queue, lock, SENTINEL, attrs)
         cv_reader.start()
         cv_detector.start()
         lock.acquire()
