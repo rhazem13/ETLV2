aa5518da21e68fbe3c73fab62d0b73d996c86fcd
youssefwael <youssefwael397>
2022-12-02 14:31:51 +0200

    7ot get pos fe detector.Py

--- app/bird_cv/BirdMoveDetect.py
+++
@@ -1,3 +0,0 @@
-class BirdMoveDetect():
-    def get_pose(self,frame):
-        return [{'headpose':1,'legpose':3,'wingpose':2,'tailpose':4},{'headpose':55,'legpose':2,'wingpose':2,'tailpose':14},{'headpose':11,'legpose':23,'wingpose':32,'tailpose':44}]
--- app/bird_cv/detector.py
+++ app/bird_cv/detector.py
@@ -3,8 +3,10 @@ import pandas as pd
 from threading import *
 import pathlib
 import os
+
+
 class CVDetector(Thread):
-    def __init__(self,cap, queue, path, sem, sentinel, attrs):
+    def __init__(self, cap, queue, path, sem, sentinel, attrs):
         super().__init__()
         self.queue = queue
         self.sentinel = sentinel
@@ -15,14 +17,25 @@ class CVDetector(Thread):
         self.attrs = attrs
         self.sem = sem
 
+    def get_pose(self, frame):
+        # Return a list of dict
+        # A dict for each bird in the frame
+        # If a frame contains one bird then the liat contains one dict
+        # [{H:R, Leg:up, T:L, w:on}]
+        # If a frame contains two birds then the list contains two dict
+        # i.e
+        # [{H:L,leg:down, T:C, W:off},
+        # {H:c, leg:up, T:R, w:on}]
+        return [{'headpose': 1, 'legpose': 3, 'wingpose': 2, 'tailpose': 4}, {'headpose': 55, 'legpose': 2, 'wingpose': 2, 'tailpose': 14}, {'headpose': 11, 'legpose': 23, 'wingpose': 32, 'tailpose': 44}]
+
     def getStats(self):
-        
+
         fps = self.cap.get(cv2.CAP_PROP_FPS)
         frame_count = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
-        
+
         codec = self.cap.get(cv2.CAP_PROP_FOURCC)
         durationSec = frame_count/fps * 1000
-        return (fps,frame_count,durationSec)
+        return (fps, frame_count, durationSec)
 
     def process(self):
         # self.sem.acquire()
@@ -38,16 +51,16 @@ class CVDetector(Thread):
                 minNeighbors=2,
                 #minSize=(10, 10),
                 maxSize=(30, 30),
-                flags = cv2.CASCADE_SCALE_IMAGE
+                flags=cv2.CASCADE_SCALE_IMAGE
             )
-            if (len(birds)>=self.MAX_NUM_BIRDS):
+            if (len(birds) >= self.MAX_NUM_BIRDS):
                 self.MAX_NUM_BIRDS = len(birds)
             frame = self.queue.get()
-            count +=1
-            if (count/frames_count)>last_percent+0.03:
+            count += 1
+            if (count/frames_count) > last_percent+0.03:
                 last_percent = count/frames_count
-                print('Processing... ',int(last_percent*100),'%')
-        print('max number is: ',self.MAX_NUM_BIRDS)
+                print('Processing... ', int(last_percent*100), '%')
+        print('max number is: ', self.MAX_NUM_BIRDS)
         data = {str(self.MAX_NUM_BIRDS)}
         df = pd.DataFrame(data, columns=['MAX_COUNT_OF_BIRDS'])
         self.attrs['df'] = df
