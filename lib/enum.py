430823bb2ef436d921c6480a3800b88c327c0fbd
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2022-12-03 15:13:16 +0200

    UpdateEnum

--- lib/enum.py
+++ lib/enum.py
@@ -1,23 +1,34 @@
 from enum import Enum
 
 
-class EHead(Enum):
-    RIGHT = 'right'
-    CENTER = 'center'
-    LEFT = 'left'
+class en_bird_part(Enum):
+    head = 0
+    leg = 1
+    wing = 2
+    tail = 3
 
 
-class ETail(Enum):
-    RIGHT = 'right'
-    CENTER = 'center'
-    LEFT = 'left'
+class en_bird_head_pose(Enum):
+    center = 0
+    right = 1
+    left = 2
+    none = -1
 
 
-class EWing(Enum):
-    ON = 'on'
-    OFF = 'off'
+class en_bird_leg_pose(Enum):
+    down = 0
+    up = 1
+    none = -1
 
 
-class ELeg(Enum):
-    UP = 'up'
-    DOWN = 'down'
+class en_bird_wing_pose(Enum):
+    off = 0
+    on = 1
+    none = -1
+
+
+class en_bird_tail_pose(Enum):
+    center = 0
+    right = 1
+    left = 2
+    none = -1
