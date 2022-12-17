0bac32dd1474350c7cfa704805be8b3a9c6520bc
rhazem13 <rhazem13@yahoo.com>
2022-12-17 23:11:54 +0200

    j

--- app/bird_cv/BirdMoveDetect.py
+++ app/bird_cv/BirdMoveDetect.py
@@ -12,18 +12,6 @@ class BirdMoveDetect:
         self.birdsCascade = cv2.CascadeClassifier(cascade_path)
         pass
 
-    def get_pose(self, frame):
-        """
-        Return a list of dict
-        A dict for each bird in the frame
-        If a frame contains one bird then the liat contains one dict
-        [{H:R, Leg:up, T:L, w:on}]
-        If a frame contains two birds then the list contains two dict
-        i.e
-        [{H:L,leg:down, T:C, W:off},
-        {H:c, leg:up, T:R, w:on}]"""
-        return [{'headpose': 1, 'legpose': 3, 'wingpose': 2, 'tailpose': 4}, {'headpose': 55, 'legpose': 2, 'wingpose': 2, 'tailpose': 14}, {'headpose': 11, 'legpose': 23, 'wingpose': 32, 'tailpose': 44}]
-
     def get_count(self, frame) -> int:
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         birds = self.birdsCascade.detectMultiScale(
